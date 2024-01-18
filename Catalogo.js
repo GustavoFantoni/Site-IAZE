bca14aae164f54987aab5fecf7b1f6841f34585b
Gustavo Fantoni <gustavo.rosario@unifebe.edu.br>
2024-01-18 14:45:29 -0300

    Adicionado menu na tela de coleções

--- Catalogo.js
+++ Catalogo.js
@@ -1,4 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+
+    let menu = document.querySelector('ul.menu-content');
+    let btn = document.querySelector('button.btn-act');
+    let overlay = document.getElementById('overlay');
+    btn.addEventListener('click', active);
+    
+    function active() {
+        menu.classList.toggle('area-menu-actived');
+        btn.classList.toggle('btn-act-active');
+    
+        overlay.classList.toggle('overlay-active');
+    
+       
+        document.body.classList.toggle('no-scroll');
+    }
+
+
+
+
+
+
     const lines = document.querySelector(".line-1");
     const lines2 = document.querySelector(".line-2");
     const lines3 = document.querySelector(".line-3");
